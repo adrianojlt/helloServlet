6c4b088479fd0e7a45faeb986aeeb9f85aaec2e3
adriano <adrianojlt@gmail.com>
2014-04-15 23:39:38 +0100

    ...

---
+++ src/pt/adrianz/helloservlet/servlets/gym/GymDispatcherServlet.java
@@ -0,0 +1,41 @@
+package pt.adrianz.helloservlet.servlets.gym;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GymDispatchServlet
+ */
+//@WebServlet("/GymDispatchServlet")
+public class GymDispatcherServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GymDispatcherServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher view = request.getRequestDispatcher("gym/insertWorkout.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
